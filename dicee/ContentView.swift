ba9d7bc90e497a5dadf408f841972df6dabd54b6
richcire <eric9907@naver.com>
2026-04-13 09:48:01 +0900

    Initial Commit

---
+++ dicee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dicee
+//
+//  Created by 연민석 on 4/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dicee/diceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  diceeApp.swift
+//  dicee
+//
+//  Created by 연민석 on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct diceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
